1ae965bdbb8c1de077695b60dd83909c64b4d6d5
seshan07 <seshanv07@gmail.com>
2025-12-20 20:46:17 +0530

    Update ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,20 +1,28 @@
 import os
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
 
-DATA_DIR = "data"
-VECTOR_DB_DIR = "vector_store"
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    DirectoryLoader
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+DATA_DIR = "data/raw_docs"
+VECTOR_DB_DIR = "data/vector_store"
 
 def load_documents():
     loader = DirectoryLoader(
         DATA_DIR,
         glob="**/*.pdf",
-        loader_cls=PyPDFLoader
+        loader_cls=PyPDFLoader,
+        silent_errors=True
     )
     documents = loader.load()
+    print(f"Loaded {len(documents)} documents")
     return documents
+
 
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter(
@@ -23,3 +31,32 @@ def split_documents(documents):
     )
     chunks = splitter.split_documents(documents)
     return
+
+def ingest():
+    documents = load_documents()
+
+    if len(documents) == 0:
+        print("No documents found. Check data path.")
+        return
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+
+    chunks = splitter.split_documents(documents)
+    print(f"Split into {len(chunks)} chunks")
+
+    embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+
+    os.makedirs(VECTOR_DB_DIR, exist_ok=True)
+    vectorstore.save_local(VECTOR_DB_DIR)
+
+    print("FAISS vector store saved successfully")
+
+
+if __name__ == "__main__":
+    ingest()
